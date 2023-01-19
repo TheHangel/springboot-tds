53f5c3c60a5180c2fe957ff56d4cb8d6d7773208
TheHangel <105165180+TheHangel@users.noreply.github.com>
2023-01-19 16:00:01 +0100

    methodes finies : addNew, inc, dec, removeItem. Reste new a faire

--- src/main/java/edu/spring/td1/controller/MainController.java
+++ src/main/java/edu/spring/td1/controller/MainController.java
@@ -14,19 +14,50 @@ public class MainController {
         return new ArrayList<>();
     }
 
+    @GetMapping("items/new")
+    public RedirectView newItem(@RequestParam String nom, @SessionAttribute("items") List<Element> items) {
+        //TODO
+        return new RedirectView("/items/");
+    }
+
     @PostMapping("items/addNew")
-    public RedirectView addNew(@RequestParam String nom) {
+    public RedirectView addNew(@RequestParam String nom, @SessionAttribute("items") List<Element> items) {
         Element e = new Element();
         e.setNom("item");
         e.setEvaluation(20);
-        this.getItems().add(e);
+        items.add(e);
+        return new RedirectView("/items/");
+    }
+
+    @GetMapping("items/inc/{nom}")
+    public RedirectView incrementer(@RequestParam String nom, @PathVariable("nom") String nomItem, @SessionAttribute("items") List<Element> items) {
+        for(Element e : items){
+            if(e.getNom().equals(nomItem)){
+                e.setEvaluation(e.getEvaluation()+1);
+            }
+        }
+        return new RedirectView("/items/");
+    }
+
+    @GetMapping("items/dec/{nom}")
+    public RedirectView decrementer(@RequestParam String nom, @PathVariable("nom") String nomItem, @SessionAttribute("items") List<Element> items) {
+        for(Element e : items){
+            if(e.getNom().equals(nomItem)){
+                e.setEvaluation(e.getEvaluation()-1);
+            }
+        }
         return new RedirectView("/items/");
     }
 
     @PostMapping("items/delete/{nom}")
     public RedirectView removeItem(@RequestParam String nom, @PathVariable("nom") String nomItem) {
 
-        //this.getItems().remove(this.getItems().get(""));
+        for(Element e : this.getItems()){
+            if(e.getNom().equals(nomItem)){
+                this.getItems().remove(e);
+            }
+        }
+
         return new RedirectView("/items/");
     }
 
