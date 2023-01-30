cb0998092809a4e0b3d579c038d26aea1a99ef59
TheHangel <105165180+TheHangel@users.noreply.github.com>
2023-01-30 23:23:20 +0100

    premiere version des catégories, sous forme de liste

--- src/main/kotlin/edu/spring/td1/model/Category.kt
+++ src/main/kotlin/edu/spring/td1/model/Category.kt
@@ -4,7 +4,7 @@ import kotlin.collections.HashSet
 
 class Category {
 
-    var nom : String? = null
+    var categoryNom : String? = null
     var items : HashSet<Item> = HashSet()
 
     companion object {
@@ -12,14 +12,14 @@ class Category {
             val categories = HashSet<Category>()
 
             val category1 = Category().apply {
-                nom = "Category 1"
+                categoryNom = "Category 1"
                 items.add(Item().apply { nom = "Item 1"; evaluation = 5 })
                 items.add(Item().apply { nom = "Item 2"; evaluation = 4 })
             }
             categories.add(category1)
 
             val category2 = Category().apply {
-                nom = "Category 2"
+                categoryNom = "Category 2"
                 items.add(Item().apply { nom = "Item 3"; evaluation = 3 })
                 items.add(Item().apply { nom = "Item 4"; evaluation = 2 })
             }
