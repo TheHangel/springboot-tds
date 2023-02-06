5cf90667c54fb7d509922a11efcabaf31c6f70c1
TheHangel <105165180+TheHangel@users.noreply.github.com>
2023-02-06 08:36:31 +0100

    annotation EnableWebMvc mise en commentaire, correction faute de frappe sur application.properties (htmlc html)

--- src/main/kotlin/edu/spring/td2/WebConfig.kt
+++ src/main/kotlin/edu/spring/td2/WebConfig.kt
@@ -6,7 +6,7 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-@EnableWebMvc
+//@EnableWebMvc
 class WebConfig : WebMvcConfigurer {
     override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
         registry
