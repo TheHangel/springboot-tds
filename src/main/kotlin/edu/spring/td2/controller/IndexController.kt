68a600410aa6ac7266f45a6a9fea9f5efcdac253
TheHangel <105165180+TheHangel@users.noreply.github.com>
2023-02-06 08:37:12 +0100

    création de l'index

---
+++ src/main/kotlin/edu/spring/td2/controller/IndexController.kt
@@ -0,0 +1,15 @@
+package edu.spring.td2.controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+
+@Controller
+@RequestMapping("")
+class IndexController {
+
+    @RequestMapping(path=["", "index"]) //deux routes possibles, soit / , soit index
+    fun index(): String {
+        return "index"
+    }
+
+}
