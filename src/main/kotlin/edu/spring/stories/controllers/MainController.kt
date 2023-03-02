ee70511a1319cd39c1d04c711db1cf7b5cfdfd49
TheHangel <105165180+TheHangel@users.noreply.github.com>
2023-03-02 15:58:34 +0100

    feat: ajout d'un developpeur a fix: affichage tableau

--- src/main/kotlin/edu/spring/stories/controllers/MainController.kt
+++ src/main/kotlin/edu/spring/stories/controllers/MainController.kt
@@ -1,16 +1,46 @@
 package edu.spring.stories.controllers
 
+import edu.spring.stories.entities.Developer
+import edu.spring.stories.repositories.DeveloperRepository
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.ui.ModelMap
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.servlet.view.RedirectView
 
 @Controller
 @RequestMapping("/")
 class MainController {
 
+    @Autowired
+    lateinit var developerRepository: DeveloperRepository
+
     //Affiche la page principale
     @RequestMapping(path = ["","index"])
-    fun indexAction():String{
+    fun indexAction(model: ModelMap):String{
+        model.addAttribute("developers", developerRepository.findAll())
         return "index"
     }
+
+    /*@PostMapping("/developer/add")
+    fun addDeveloper(@RequestParam firstname: String, @RequestParam lastname: String): RedirectView {
+        val developer = Developer(firstname, lastname)
+        developerRepository.save(developer)
+        return RedirectView("/")
+    }*/
+
+    @PostMapping("/developer/add")
+    fun submitNewAction(
+        @ModelAttribute dev:Developer
+    ):RedirectView{
+        developerRepository.save(dev)
+        return RedirectView("/")
+    }
+
+    /*@PostMapping("/developer/add")
+    fun addDeveloper(@ModelAttribute developer: Developer): RedirectView {
+        developerRepository.save(developer)
+        return RedirectView("/")
+    }*/
 
 }
