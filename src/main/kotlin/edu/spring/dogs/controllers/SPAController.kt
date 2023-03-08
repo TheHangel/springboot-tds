8b4db671c8536a6ec0eaa23b11372344397be19b
TheHangel <105165180+TheHangel@users.noreply.github.com>
2023-03-08 23:31:39 +0100

    avancement spacontroller

--- src/main/kotlin/edu/spring/dogs/controllers/SPAController.kt
+++ src/main/kotlin/edu/spring/dogs/controllers/SPAController.kt
@@ -4,6 +4,7 @@ import edu.spring.dogs.repositories.DogRepository
 import edu.spring.dogs.repositories.MasterRepository
 import io.github.jeemv.springboot.vuejs.VueJS
 import io.github.jeemv.springboot.vuejs.utilities.Http
+import io.github.jeemv.springboot.vuejs.utilities.JsArray
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
@@ -31,6 +32,29 @@ class SPAController {
         vue.addData("message", "Hello Vue.js!")
         vue.addDataRaw("masters", "[]")
         vue.addData("dogs", dogRepository.findByMasterIsNull())
+        vue.addData("master", "pas de nom")
+        vue.addMethod("addMaster",
+            "Http.post('/master/add',this.master,Http.responseArrayToArray('this.masters','masters'))"
+        )
+        vue.addMethod("remove",
+            Http.delete("'/masters/'+master.id",
+                JsArray.remove("this.masters","master")+
+                        JsArray.addAll("this.dogs","master.dogs")+
+                        "console.log(`Maître \${master.firstname} supprimé!`);",
+                "console.log('Erreur sur suppression de master!');"
+            ),
+            "master")
+        vue.addDataRaw("message","{title:'',content:''}")
+        vue.addMethod("showMessage",
+            "this.message={error: error,title: title, content: content, display: true};"+
+                    "setTimeout(function(){this.message.display=false;}.bind(this),5000);",
+            "title","content","error")
+        vue.addMethod("successMessage",
+            "this.showMessage(title,content,false);",
+            "title","content")
+        vue.addMethod("errorsMessage",
+            "this.showMessage(title,content,true);",
+            "title","content")
         vue.onMounted(Http.get("/masters",
                 Http.responseArrayToArray("this.masters","masters")
             )
