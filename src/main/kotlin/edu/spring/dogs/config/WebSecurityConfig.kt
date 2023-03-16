f3594052552741eff5acc40766f4aff597d5d7c9
TheHangel <105165180+TheHangel@users.noreply.github.com>
2023-03-16 16:28:31 +0100

    fin Spring security, début TD5 (le vrai)

--- src/main/kotlin/edu/spring/dogs/config/WebSecurityConfig.kt
+++ src/main/kotlin/edu/spring/dogs/config/WebSecurityConfig.kt
@@ -1,9 +1,13 @@
 package edu.spring.dogs.config
 
+import edu.spring.dogs.services.DbUserService
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
@@ -12,13 +16,30 @@ class WebSecurityConfig {
     @Bean
     @Throws(Exception::class)
     fun configure(http: HttpSecurity): SecurityFilterChain {
+        http.authorizeHttpRequests { authorizeHttpRequests ->
+            authorizeHttpRequests.requestMatchers(PathRequest.toH2Console()).permitAll()
+            authorizeHttpRequests.requestMatchers("/login").permitAll()
+            authorizeHttpRequests.requestMatchers("/init/**", "/masters/**", "/dogs/**", "/css/**", "/images/**",    "/h2-console/**", "/login/**").permitAll()
+            authorizeHttpRequests.requestMatchers("/master/**").hasAuthority("USER")
+            authorizeHttpRequests.anyRequest().authenticated()
+        }
         http
-            .csrf().disable().authorizeHttpRequests()
-            .requestMatchers("/master/**").hasRole("manager")
-            .anyRequest().authenticated()
+            .csrf().disable()
+            .formLogin().loginPage("/login")
+            .successForwardUrl("/")
             .and()
-            .formLogin()
+            .headers().frameOptions().sameOrigin()
         return http.build()
+    }
+
+    @Bean
+    fun userDetailsService(): UserDetailsService? {
+        return DbUserService()
+    }
+
+    @Bean
+    fun bCryptPasswordEncoder(): BCryptPasswordEncoder? {
+        return BCryptPasswordEncoder()
     }
 
 }
